7ed599724b3ae4064289151cf0e9e79233884e3b
Akustik <akustik@hey.com>
2024-06-22 21:37:30 +0200

    derive key and iv

--- wolfy/wolfy/main.py
+++ wolfy/wolfy/main.py
@@ -4,8 +4,10 @@ import time
 import base64
 import os.path
 
-from wolfcrypt.hashes import Sha256
 from wolfcrypt.ciphers import Aes, MODE_CBC 
+from wolfcrypt.hashes import Sha512
+
+DEFAULT_ROUNDS = 1024 * 1024
 
 @click.group()
 def cli():
@@ -13,14 +15,15 @@ def cli():
 
 @cli.command()
 @click.option('--password', type=str)
-@click.option('--iv', type=str)
+@click.option('--salt', type=str)
+@click.option('--rounds', default=DEFAULT_ROUNDS, type=int)
 @click.option('-a', '--alg', type=click.Choice(['AES'], case_sensitive=False), default='AES')
 @click.option('-k', '--key', required=True, type=str)
 @click.option('-v', '--value', required=True, type=str)
 @click.option('-o', '--output', default='-', type=str)
 @click.option('-i', '--input', type=str)
-def put(password, iv, alg, key, value, input, output):
-    data = _load_data_from_input(password, iv, input)
+def put(password, salt, alg, rounds, key, value, input, output):
+    data = _load_data_from_input(password, salt, rounds, input)
     updated = False
 
     for entry in data:
@@ -37,37 +40,44 @@ def put(password, iv, alg, key, value, input, output):
             "last_updated": time.ctime()  
         })
 
-    _dump_data_to_output(password, iv, output, data)
+    _dump_data_to_output(password, salt, rounds, output, data)
 
 @cli.command()
 @click.option('--password', type=str)
-@click.option('--iv', type=str)
+@click.option('--salt', type=str)
+@click.option('--rounds', default=DEFAULT_ROUNDS, type=int)
 @click.option('-a', '--alg', type=click.Choice(['AES'], case_sensitive=False), default='AES')
 @click.option('-p', '--pattern', required=True, type=str)
 @click.option('-i', '--input', default='-', type=str)
-def query(password, iv, alg, pattern, input):
-    data = _load_data_from_input(password, iv, input)
+def query(password, salt, alg, rounds, pattern, input):
+    data = _load_data_from_input(password, salt, rounds, input)
     matches = list(filter(lambda x: pattern in x['key'] or pattern in x['value'], data))
     for m in matches:
         click.echo(f'{m}')
 
 
-def _load_data_from_input(password, iv, input):
+def _load_data_from_input(password, salt, rounds, input):
+    key = _derive(password, 32, rounds)
+    iv = _derive(salt, 16, rounds)
+
     if not input:
         return []
 
     with click.open_file(input, 'rb') as f:
         file_content = f.read()
      
-    cipher = Aes(password.encode(), MODE_CBC, iv.encode())
+    cipher = Aes(key, MODE_CBC, iv)
     cipher_text = base64.b64decode(file_content)
     yaml_data = cipher.decrypt(cipher_text)
     data = yaml.safe_load(yaml_data)
     return data
 
 
-def _dump_data_to_output(password, iv, output, data):
-    cipher = Aes(password.encode(), MODE_CBC, iv.encode())
+def _dump_data_to_output(password, salt, rounds, output, data):
+    key = _derive(password, 32, rounds)
+    iv = _derive(salt, 16, rounds)
+
+    cipher = Aes(key, MODE_CBC, iv)
     yaml_data = yaml.dump(data)
     partial = len(yaml_data) % 16
     if partial != 0:
@@ -78,6 +88,12 @@ def _dump_data_to_output(password, iv, output, data):
 
     with click.open_file(output, 'wb') as f:
         f.write(file_content)
+
+
+def _derive(input, length, rounds): 
+    for i in range(0,rounds):
+        input = Sha512(input).digest()
+    return Sha512(input).hexdigest()[0: length]
 
 if __name__ == '__main__':
     cli(auto_envvar_prefix='WOLFY')
