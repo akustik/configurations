1f453bfc0ca62dcc5909b92cd793c2fcd33b1c7f
Akustik <akustik@hey.com>
2024-07-04 11:55:29 +0200

    Use random IV. Use chacha by default

--- wolfy/wolfy/main.py
+++ wolfy/wolfy/main.py
@@ -6,6 +6,7 @@ import os.path
 
 from wolfcrypt.ciphers import Aes, ChaCha, MODE_CBC 
 from wolfcrypt.hashes import Sha512
+from wolfcrypt.random import Random
 
 @click.group()
 def cli():
@@ -13,15 +14,14 @@ def cli():
 
 @cli.command()
 @click.option('--password', type=str)
-@click.option('--salt', type=str)
 @click.option('--rounds', type=int)
-@click.option('-a', '--alg', type=click.Choice(['AES', 'CHACHA']), default='AES')
+@click.option('-a', '--alg', type=click.Choice(['AES', 'CHACHA']), default='CHACHA')
 @click.option('-k', '--key', required=True, type=str)
 @click.option('-v', '--value', required=True, type=str)
 @click.option('-o', '--output', default='-', type=str)
 @click.option('-i', '--input', type=str)
-def put(password, salt, alg, rounds, key, value, input, output):
-    algorithm = _key_algorithm_for(alg, password, salt, rounds or 0)
+def put(password, alg, rounds, key, value, input, output):
+    algorithm = _key_algorithm_for(alg, password, rounds or 0)
     data = _load_data_from_input(algorithm, input)
     updated = False
 
@@ -43,13 +43,12 @@ def put(password, salt, alg, rounds, key, value, input, output):
 
 @cli.command()
 @click.option('--password', type=str)
-@click.option('--salt', type=str)
 @click.option('--rounds', type=int)
-@click.option('-a', '--alg', type=click.Choice(['AES', 'CHACHA']), default='AES')
+@click.option('-a', '--alg', type=click.Choice(['AES', 'CHACHA']), default='CHACHA')
 @click.option('-p', '--pattern', required=True, type=str)
 @click.option('-i', '--input', default='-', type=str)
-def query(password, salt, alg, rounds, pattern, input):
-    algorithm = _key_algorithm_for(alg, password, salt, rounds or 0)
+def query(password, alg, rounds, pattern, input):
+    algorithm = _key_algorithm_for(alg, password, rounds or 0)
     data = _load_data_from_input(algorithm, input)
     matches = list(filter(lambda x: pattern in x['key'] or pattern in x['value'], data))
     for m in matches:
@@ -62,7 +61,10 @@ def _load_data_from_input(algorithm, input):
 
     with click.open_file(input, 'rb') as f:
         file_content = f.read()
-    cipher_text = base64.b64decode(file_content)
+    iv = base64.b64decode(file_content.split(b'.')[0])
+    cipher_text = base64.b64decode(file_content.split(b'.')[1])
+
+    algorithm.set_iv(iv)
     yaml_data = algorithm.decrypt(cipher_text)
     data = yaml.safe_load(yaml_data)
     return data
@@ -70,18 +72,19 @@ def _load_data_from_input(algorithm, input):
 
 def _dump_data_to_output(algorithm, output, data):
     yaml_data = yaml.dump(data)
+    algorithm.init_iv()
     cipher_text = algorithm.encrypt(yaml_data)
-    file_content = base64.b64encode(cipher_text)
+    file_content = base64.b64encode(algorithm.get_iv()) + b'.' + base64.b64encode(cipher_text)
 
     with click.open_file(output, 'wb') as f:
         f.write(file_content)
 
 
 class KeyAlgorithm:
-    def __init__(self, password, salt, rounds, key_length, iv_length):
+    def __init__(self, password, rounds, key_length, iv_length):
         self.key = _derive(password, key_length, rounds)
         self.key_length = key_length
-        self.iv = _derive(salt, iv_length, rounds)
+        self.iv_length = iv_length
 
     def encrypt(self, plain_text):
         pass 
@@ -89,9 +92,19 @@ class KeyAlgorithm:
     def decrypt(self, encrypted_text):
         pass 
 
+    def get_iv(self):
+        return self.iv
+
+    def set_iv(self, iv):
+        self.iv = iv
+
+    def init_iv(self):
+        self.iv = Random().bytes(self.iv_length)
+
+
 class AESAlgorithm(KeyAlgorithm):
-    def __init__(self, password, salt, rounds):
-        super().__init__(password, salt, max(rounds, 1024 * 1024), 32, 16)
+    def __init__(self, password, rounds):
+        super().__init__(password, max(rounds, 1024 * 1024), 32, 16)
 
     def encrypt(self, plain_text):
         partial = len(plain_text) % 16
@@ -103,8 +116,8 @@ class AESAlgorithm(KeyAlgorithm):
         return Aes(self.key, MODE_CBC, self.iv).decrypt(cipher_text) 
 
 class ChaChaAlgorithm(KeyAlgorithm):
-    def __init__(self, password, salt, rounds):
-        super().__init__(password, salt, max(rounds, 1024 * 1024), 32, 16)
+    def __init__(self, password, rounds):
+        super().__init__(password, max(rounds, 1024 * 1024), 32, 16)
 
     def encrypt(self, plain_text):
         cipher = ChaCha(self.key, self.key_length)
@@ -120,11 +133,11 @@ class ChaChaAlgorithm(KeyAlgorithm):
         cipher.mode = 'unused'
         return cipher.decrypt(cipher_text)         
 
-def _key_algorithm_for(alg, password, salt, rounds):
+def _key_algorithm_for(alg, password, rounds):
     if alg == 'CHACHA':
-        return ChaChaAlgorithm(password, salt, rounds)
+        return ChaChaAlgorithm(password, rounds)
     elif alg == 'AES':
-        return AESAlgorithm(password, salt, rounds)
+        return AESAlgorithm(password, rounds)
     else:
         raise Exception("Unknown algorithm")
 
